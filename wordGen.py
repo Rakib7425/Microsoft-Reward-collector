32fd6291c2c5c500dcd8c567ddb1ff2d8622adb7
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-04-08 23:35:57 +0530

    new file:   Random.py 	new file:   wordGen.py

---
+++ wordGen.py
@@ -0,0 +1,12 @@
+# import random
+# words = ["apple", "banana", "cherry", "date", "elderberry"]
+# random_words = [random.choice(words) for _ in range(5)]
+
+# print(random_words)
+
+
+# from random_word import RandomWords
+# r = RandomWords()
+# random_words = r.get_random_words(limit=5)
+
+# print(random_words)
