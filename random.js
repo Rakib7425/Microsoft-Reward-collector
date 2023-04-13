610b6ad14347810276d224ae6ed27b328165b1ee
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-04-13 16:45:46 +0530

    modified:   random.html 	modified:   random.js

--- random.js
+++ random.js
@@ -1,19 +1,32 @@
 
-let word = document.getElementById(wordGen);
+let getReward = document.getElementById('btn');
+let word = document.getElementById('wordGen');
+let doc = document.getElementById('doc');
+
+let array = [];
+
 
 word.addEventListener('click', () => {
-    let words = [];
     for (let i = 0; i < 100; i++) {
         let word = '';
         const letters = 'abcdefghijklmnopqrstuvwxyz';
+        // console.log(letters.toUpperCase());
         const length = Math.floor(Math.random() * 6) + 5;
         for (let j = 0; j < length; j++) {
             word += letters.charAt(Math.floor(Math.random() * letters.length));
         }
-        words.push(`"${word}"`);
+        array.push(`"${word}"`);
     }
-    // console.log(words.join(', '));
-
-    document.write(words.join(', '))
+    
+    doc.innerHTML = array.join(', ')
 })
 
+getReward.addEventListener('click', ()=>{
+    array.forEach((element, index) => {
+        setTimeout(() => {
+            console.log(element);
+            // window.open("https://www.google.com/search?q=" + element); // Google Chrome
+            window.open("https://www.bing.com/search?q=" + element); // Microsoft Edge
+        }, 5000);
+    });
+})
