2a7b771d937da9b2f1ad27eed19be8c272351ebd
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-04-13 16:15:11 +0530

    modified:   wordGen.py

--- wordGen.py
+++ wordGen.py
@@ -10,3 +10,12 @@
 # random_words = r.get_random_words(limit=5)
 
 # print(random_words)
+
+import random
+
+words = []
+for i in range(100):
+    word = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=random.randint(5, 10)))
+    words.append(f'"{word}"')
+
+print(', '.join(words))
