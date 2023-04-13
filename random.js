771d3622d6c96cc5ff2f51845d6ace12da45f0e5
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-04-13 17:28:52 +0530

    modified:   random.html 	modified:   random.js

--- random.js
+++ random.js
@@ -1,33 +1,48 @@
 
-let getReward = document.getElementById('btn');
+let getReward = document.getElementById('getReward');
 let word = document.getElementById('wordGen');
-let doc = document.getElementById('doc');
+let div = document.getElementById('doc');
 
 
 let array = [];
 
+let index = 0;
+
 word.addEventListener('click', () => {
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 10; i++) {
         let word = '';
         const letters = 'abcdefghijklmnopqrstuvwxyz';
-        // console.log(letters.toUpperCase());
-        const length = Math.floor(Math.random() * 6) + 5;
+        const length = Math.floor(Math.random() * 5) + 5;
         for (let j = 0; j < length; j++) {
             word += letters.charAt(Math.floor(Math.random() * letters.length));
         }
         array.push(`"${word}"`);
     }
-    
-    doc.innerHTML = array.join(', ')
+
+    div.innerText = array.join(', ')
 })
 
-getReward.addEventListener('click', ()=>{
-    array.forEach((element, index) => {
-        setTimeout(() => {
-            console.log(element);
-            // window.open("https://www.google.com/search?q=" + element); // Google Chrome
 
-            window.open("https://www.bing.com/search?q=" + element); // Microsoft Edge
-        }, 5000);
-    });
+// TODO 
+if (array.length <= 1) {
+    getReward.style.pointerEvents="none";
+    getReward.style.backgroundColor = "green";
+} else {
+    getReward.style.cursor="default";
+    getReward.style.background = rgb(6, 171, 146);
+}
+
+
+getReward.addEventListener('click', () => {
+
+    var intervalId = setInterval(function () {
+        console.log(array[index]);
+        // window.open("https://www.google.com/search?q=" + array[index]); // Google Chrome
+        // window.open("https://www.bing.com/search?q=" + array[index]); // Microsoft Edge
+        index++;
+        if (index >= array.length) {
+            clearInterval(intervalId);
+        }
+    }, 1000);
+
 })
